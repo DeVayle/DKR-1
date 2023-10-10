adce9a041f1c44dad77caebbe0a24979c7b8c38a
DeVayle <144678017+DeVayle@users.noreply.github.com>
2023-10-10 18:10:57 +0300

    Add files via upload

---
+++ dkr1_1.pas
@@ -0,0 +1,14 @@
+﻿program dkr1_1;
+
+var
+  x, y: real;
+  
+begin
+  writeln('Введите x');
+  readln(x);
+  if (x<-10) then y := ln(x)/ln(10)
+    else
+  if (x<=-5) then y := tan(x)-tan(x)
+    else y := cos(2*x)-cos(2*x);
+    writeln(y)
+end.
---
+++ dkr1_2.pas
@@ -0,0 +1,16 @@
+﻿program dkr1_2;
+
+var
+  x, y: real;
+  
+begin
+  x := -12;
+  while (x<=-3) do
+  begin
+    if (x<-10) then writeln('Для х = ', x:0:1,', у = ', y := ln(x)/ln(10))
+    else
+  if (x<=-5) then writeln('Для х = ', x:0:1,', у = ', y := tan(x)-tan(x))
+    else writeln('Для х = ', x:0:1,', у = ', y := cos(2*x)-cos(2*x));
+    x := x+0.2;
+  end;
+end.
